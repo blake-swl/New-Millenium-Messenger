4f475298c75a8cef8554dc79bf0b0f5b25855955
blake-swl <seulwoolee93@gmail.com>
2018-08-14 17:08:47 -0700

    set up front-end dev environment

---
+++ client/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+export default class App extends Component {
+  constructor() {
+    super();
+
+  }
+  render() {
+    return(
+      <div>
+        This is form App component app.jsx
+      </div>
+    )
+  }
+}
