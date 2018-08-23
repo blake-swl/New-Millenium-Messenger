103e019b85c9bba47cc0aa84c2d11a9d36f4f1e3
blake-swl <seulwoolee93@gmail.com>
2018-08-23 15:54:43 -0700

    added dependancies for graphql

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const parser = require('body-parser');
+const { graphqlExpress } = require('apollo-server-express');
 
 const app = express();
 const PORT = process.env.PORT || 9000;
@@ -13,9 +14,8 @@ app.use(parser.json());
 app.use(parser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, "../client/dist/")));
 
-// const app = require('express')();
-// const http = require('http').Server(app);
-// const io = require('socket.io')(http);
+
+
 server.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
 
@@ -24,23 +24,3 @@ server.listen(PORT, () => {
   })
 })
 
-
-// GET /status
-// app.get('/', function(req, res) {
-//   res.sendFile(__dirname, '../client/dist/index.html')
-// })
-
-// io.on('connection', () => console.log('a user connected'));
-
-
-
-
-// app.use('/', router);
-
-
-// Socket setup
-// let io = socket(server);
-
-// io.on('connection', (socket) => {
-//   console.log('connected to socket')
-// })
